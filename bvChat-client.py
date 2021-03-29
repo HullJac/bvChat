2a8afbb74d67fb996f1df293ceb41cdcd92b63e6
haafgre <haafgre@bvu.edu>
2021-03-28 20:53:20 -0500

    Finished, need small touches

--- bvChat-client.py
+++ bvChat-client.py
@@ -17,15 +17,16 @@ serverIP = sys.argv[1]
 serverPort = int(sys.argv[2])
 
 listener = ''
+inp = ""
+serverSock = 0
 
 def recMSG(connInfo):
-    print()
     serverConn, serverAddr = connInfo
     msg = "a"
     while len(msg) != 0:
         msg = getLine(serverConn)
         if len(msg) != 0:
-            print(msg)
+            print(msg + "\n")
         else:
             pass
 
@@ -62,81 +63,89 @@ def listenToServer():
         listener.close()
 
 def userInput(serverIP, serverPort):
-    global listenPort
+    global listenPort, run, inp, serverSock
     logged = False
     while not logged:
-        serverSock = connectToServer(serverIP, serverPort) 
-        listenPort = str(listenPort) + "\n"
-        serverSock.send(listenPort.encode())
-        uName = input("Username: ")
-        pWord = input("Password: ")
+        try:
+            serverSock = connectToServer(serverIP, serverPort) 
+            listenPort = str(listenPort) + "\n"
+            serverSock.send(listenPort.encode())
+            uName = ''
+            while uName == '':
+                uName = input("Username: ")
 
-        print("name: " + uName + " word: " + pWord)
-        print("-------------------------------------------------------")
-        accountName = uName+"\n" 
-        accountWord = pWord+"\n"
-        serverSock.send(accountName.encode())
-        serverSock.send(accountWord.encode())
-        
-        code = getLine(serverSock)
-        print(code)
-        if code == "old":
+            pWord = ''
+            while pWord == '':
+                pWord = input("Password: ")
+            
             print("-------------------------------------------------------")
-            offlineInt = int(getLine(serverSock))
-            if offlineInt > 0:
-                for i in range(offlineInt):
-                    print(getLine(serverSock))
+            print("> ", end = '')
+            accountName = uName+"\n" 
+            accountWord = pWord+"\n"
+            #print("name: " + "#" + accountName + "#" + " word: " + "#" + accountWord+ "#")
+
+            serverSock.send(accountName.encode())
+            serverSock.send(accountWord.encode())
+            
+            code = getLine(serverSock)
+            if code == "old":
+                print("-------------------------------------------------------")
+                offlineInt = int(getLine(serverSock))
+                if offlineInt > 0:
+                    for i in range(offlineInt):
+                        print(getLine(serverSock))
+                else:
+                    print("There were no offline messages")
+                print("-------------------------------------------------------")
+                entryMSG = getLine(serverSock)
+                print(entryMSG)
+                motd = getLine(serverSock)
+                print("Message Of The Day: " + motd)
+                logged = True
+            elif code == "new":
+                entryMSG = getLine(serverSock)
+                print(entryMSG)
+                motd = getLine(serverSock)
+                print("Message Of The Day: " + motd)
+                logged = True
+            elif code == "ban":
+                print(" [Failed 3 Attempts. Account is now temporarily banned for 2 minutes]")
+                print(" [Rerun program after time is up.] \n {Closing Program}")
+                run = False
+                serverSock.close()
+            elif code == "trying":
+                print(" [Bad Attempt] \n [Rerun program to try again] \n {Closing Program}")
+                run = False
+                serverSock.close()
+                sys.exit()
             else:
-                print("There were no offline messages")
-            print("-------------------------------------------------------")
-            entryMSG = getLine(serverSock)
-            print(entryMSG)
-            motd = getLine(serverSock)
-            print("Message Of The Day: " + motd)
-            logged = True
-        elif code == "new":
-            entryMSG = getLine(serverSock)
-            print(entryMSG)
-            motd = getLine(serverSock)
-            print("Message Of The Day: " + motd)
-            logged = True
-        elif code == "ban":
-            print("Failed 3 Attempts. Account is now temporarily banned for 1 minute")
-            serverSock.close()
-        elif code == "trying":
-            print("Bad Attempt")
-            serverSock.close()
-            #sys.exit()
-        else:
-            print("Account in use.")
-            serverSock.close()
+                print(" [Account in use.]")
+                serverSock.close()
+        except:
+            print(" [Your IP is temporarily banned, can't login to any accounts at this moment.]")
+            print(" [Rerun program after time is up.] \n {Closing Program}")
+            run = False
 
     exit = False
-    global run
     while not exit:
-        print("-------------------------------------------------------")
-        inp = input("> ")
-        message = inp+"\n"
-        serverSock.send(message.encode())
-        inp.lower()
-
         try:
+            print("-------------------------------------------------------")
+            inp = input("> ")
+            message = inp+"\n"
+            serverSock.send(message.encode())
+            inp.lower()
             if inp == "/exit":
-                serverSock.send(inp.encode())
-                print("inside try /exit")
                 serverSock.close()
                 exit = True
                 run = False
-                #sys.exit(0)
         except KeyboardInterrupt:
-            # FIXME print out exit, and exit the user account
-            print("first kb interrupt")
-            print('\n[Shutting down]')
-            #listener.close()
-            serverSock.close()
-            exit = True
-            run = False
-            #sys.exit(0) 
+           # FIXME print out exit, and exit the user account
+           print('\n[Shutting down]')
+           msg = "/exit\n"
+           serverSock.send(msg.encode())
+           serverSock.close()
+           exit = True
+           run = False
     
 
 
@@ -155,10 +164,10 @@ while run:
     try:
         pass
     except KeyboardInterrupt:
-        # FIXME print out exit, and exit the user account
+        msg = "/exit\n"
+        serverSock.send(msg.encode())
         print("second kb interrupt")
         print('\n[Shutting down]')
-        #listener.close()
-        #serverSock.close()
         sys.exit(0)
-sys.exit(0)
+print("Hi")
+sys.exit(0) 
